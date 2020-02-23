36047c2805afaa68ddccff708be32cb281d918d7
Nadezhda Brazhnikova <b.nadejda.v@mail.ru>
2020-02-23 04:25:54 +0300

    Оживление формы, префиксы, минимизация

--- js/search-form.js
+++ js/search-form.js
@@ -3,19 +3,29 @@ var popup = document.querySelector(".search-form");
 var form = document.querySelector("form");
 var arrival = popup.querySelector("[name=date-in]");
 var leave = popup.querySelector("[name=date-out]");
+var adults = popup.querySelector("[name=amount-adults]");
+var children = popup.querySelector("[name=amount-children]");
+
+popup.classList.add("search-form-none");
 
 button.addEventListener("click", function (evt) {
   evt.preventDefault();
-  popup.classList.toggle("search-form-none");
+  popup.classList.toggle("search-form-down");
   arrival.focus();
 
 });
 
 form.addEventListener("submit", function (evt) {
-	if (!arrival.value || !leave.value) {
-  evt.preventDefault();
-  popup.classList.remove("search-form-error");
-  popup.offsetWidth = popup.offsetWidth;
-	popup.classList.add("search-form-error");
-	}
+  if (!arrival.value || !leave.value || !adults.value || !children.value) {
+    evt.preventDefault();
+    popup.classList.remove("search-form-error");
+    popup.offsetWidth = popup.offsetWidth;
+    popup.classList.add("search-form-error");
+  } else {
+    if (isStorageSupport) {
+      localStorage.setItem("amount-adults", amount-adults.value);
+      localStorage.setItem("amount-children", amount-adults.value);
+    }
+    }
+
 });
