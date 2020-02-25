ace9a91039541f140c9dfc96e9c4239bb09f4820
Nadezhda Brazhnikova <b.nadejda.v@mail.ru>
2020-02-25 10:37:46 +0300

    Сохранение в Storage, мелкие исправления

--- js/search-form.js
+++ js/search-form.js
@@ -5,13 +5,39 @@ var arrival = popup.querySelector("[name=date-in]");
 var leave = popup.querySelector("[name=date-out]");
 var adults = popup.querySelector("[name=amount-adults]");
 var children = popup.querySelector("[name=amount-children]");
+var isStorageSupport = true;
+var storageArrival = "";
+var storageLeave = "";
+var storageAdults = "";
+var storageChildren = "";
 
 popup.classList.add("search-form-none");
+
+try {
+  storageArrival = localStorage.getItem("arrival");
+  storageLeave = localStorage.getItem("leave");
+  storageAdults = localStorage.getItem("adults");
+  storageChildren = localStorage.getItem("children");
+} catch (err) {
+  isStorageSupport = false;
+}
 
 button.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.toggle("search-form-down");
   arrival.focus();
+  if (storageArrival) {
+    arrival.value = storageArrival;
+  }
+  if (storageLeave) {
+    leave.value = storageLeave;
+  }
+  if (storageAdults) {
+    adults.value = storageAdults;
+  }
+  if (storageChildren) {
+    children.value = storageChildren;
+  }
 
 });
 
@@ -23,9 +49,11 @@ form.addEventListener("submit", function (evt) {
     popup.classList.add("search-form-error");
   } else {
     if (isStorageSupport) {
-      localStorage.setItem("amount-adults", amount-adults.value);
-      localStorage.setItem("amount-children", amount-adults.value);
+      localStorage.setItem("arrival", arrival.value);
+      localStorage.setItem("leave", leave.value);
+      localStorage.setItem("adults", adults.value);
+      localStorage.setItem("children", children.value);
     }
-    }
+  }
 
 });
